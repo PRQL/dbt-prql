591d9cfcaaf26f1d0ae96a7260cd3efbc320f1ab
Maximilian Roos <m@maxroos.com>
2022-06-25 20:19:58 -0700

    Update verison of prql-python

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ export PATH=$PYTHON_PATH_:$PATH
 export DBT_PRQL_DISABLE=1
 export DBT_PRQL_LOG_LEVEL=10
 cd ${test_dbt_project}
-pip install ../../dbt-prql 
-dbt clean 
-dbt deps 
+pip install ../../dbt-prql
+dbt clean
+dbt deps
 cd ~/workspace/dbt-prql/
 echo "🟢"
